1942c825dd5d0c7c8e8a44efaff3c2c0711043e5
Scrutch <estelle.moncollin@gmail.com>
2014-12-05 05:04:53 +0100

    Correction d'une erreur sur la quatrième image

--- client/js/question.js
+++ client/js/question.js
@@ -1,2 +1,2 @@
 var question=prompt("Bonjour et bienvenue sur ce site humanitaire. Comment vous appelez-vous?");
-alert('Enchanté '+ question +'. Dans cette page vous trouverez quelques abérrations, j\'espère que cela ne gênera pas votre visite sur notre site :)');
+alert('Enchanté '+ question +'. Dans cette page vous trouverez quelques aberrations, j\'espère que cela ne gênera pas votre visite sur notre site :)');
